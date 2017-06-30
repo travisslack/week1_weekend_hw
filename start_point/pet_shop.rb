4f6e25281132ad9b2efe2c7a8ff95b4871922067
Travis Grimmond <travisslack1994@outlook.com>
2017-06-30 11:45:59 +0100

    add or remove cash passing

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,3 +1,12 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  return pet_shop[:admin][:total_cash] += amount
+
+end
