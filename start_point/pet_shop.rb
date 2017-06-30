ea717b7b5a778ef815fc380527b7e4cd29380b7e
Travis Grimmond <travisslack1994@outlook.com>
2017-06-30 14:12:59 +0100

    Weekend H Q7

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -10,3 +10,20 @@ def add_or_remove_cash(pet_shop, amount)
   return pet_shop[:admin][:total_cash] += amount
 
 end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end 
+
+def increase_pets_sold(pet_shop, amount)
+  return pet_shop[:admin][:pets_sold] += 2
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
+def pets_by_breed(pet_shop, breed)
+return count
+end
+
